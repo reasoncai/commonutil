2755722555609750a303f97e85b9afa55696af1f
reasoncai <403411876@qq.com>
2017-07-02 17:37:58 +0800

    httpclient

---
+++ src/main/java/com/cai/commonutil/http/httpclient/HttpTest.java
@@ -0,0 +1,40 @@
+package com.cai.commonutil.http.httpclient;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+/**
+ * Created by reason on 17/6/9.
+ */
+public class HttpTest {
+    public static void main(String[] args) {
+        HttpResponse response = null;
+        try {
+            HttpGet get = new HttpGet("http://203.110.166.69:8080/web-ext/cop/callback/startservice");
+            response = HttpClientUtil.getHttpClient().execute(get);
+            if(response.getStatusLine().getStatusCode() != HttpStatus.SC_OK){
+                EntityUtils.consume(response.getEntity());
+                //error
+            } else {
+                String result = EntityUtils.toString(response.getEntity());
+                System.out.println(result);
+                //ok
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            if(response != null){
+                try {
+                    EntityUtils.consume(response.getEntity());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
